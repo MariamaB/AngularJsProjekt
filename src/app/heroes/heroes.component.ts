b370acd64113a5539ec9aaccf87852059b47d732
MariamaB <candy91.mb@googlemail.com>
2018-09-15 23:20:30 +0200

    List updates view Angular poject Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +8,23 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };  constructor() { }
+  /*Globale variable hero mit einem Hero Objekt initialisieren und das Objekt
+  instanziiert*/
+  hero: Hero = {id: 1, name: 'Windstorm'};
+
+  /*Globale Variable heroes mit mit der mock-heroes liste initialisiert*/
+  heroes = HEROES;
+
+ /*Globale Variable selectedHero mit dem Tye Hero deklariert*/
+  selectedHero: Hero;
+
+  /*onSelect Methode befüllt beim ausführen die globale Variable selectedHero
+   mit dem ausgewählten hero Objekt*/
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
 
   ngOnInit() {
   }
